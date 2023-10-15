fa94b298dec6d342c46b1eb644deb334c7a21af6
Roudi22 <roudi.alzkemi@gmail.com>
2023-10-15 22:45:35 +0200

    styling the nav componenet

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,28 @@
+import {headerLogo}  from "../assets/images"
+import { hamburger } from "../assets/icons"
+// importing the links data from the index.js in constants folder
+import { navLinks } from "../constants"
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header className="padding-x py-8 absolute z-10 w-full">
+        <nav className="flex border-2 justify-between items-center max-container">
+            <a href="/">
+                <img src={headerLogo} alt="logo" width={130} height={29}/>
+            </a>
+            <ul className="flex justify-center items-center gap-16 max-[815px]:hidden">
+                
+                { // mapping through the navlinks to display all of them
+                    navLinks.map((item) => (
+                        <li key={item.label}><a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">{item.label}</a></li>
+                    ))
+                }
+            </ul>
+            {/* Display the menu icon on small devices */}
+            <div className="hidden max-[815px]:block">
+                <img src={hamburger} alt="hamburger-menu" width={25} height={25}/>
+            </div>
+        </nav>
+    </header>
   )
 }
 
