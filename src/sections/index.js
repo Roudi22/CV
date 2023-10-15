4dcf6c580c036c3057101ae6d57e93379ab93587
Roudi22 <roudi.alzkemi@gmail.com>
2023-10-15 19:16:12 +0200

    Add some comments

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,3 +1,4 @@
+// Importing the components from their files
 import Hero from "./Hero"
 import Footer from "./Footer"
 import CustomerReviews from "./CustomerReviews"
@@ -6,7 +7,7 @@ import Services from "./Services"
 import SpecialOffer from "./SpecialOffer"
 import Subscribe from "./Subscribe"
 import SuperQuality from "./SuperQuality"
-
+// exporting the components
 export {
     Hero,
     PopularProducts,
