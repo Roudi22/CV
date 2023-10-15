9df4fd7ab06d476e01feb41eaa44ddc796b1b43d
Roudi22 <roudi.alzkemi@gmail.com>
2023-10-15 19:37:28 +0200

    using the components and importing Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
+import Nav from "./components/Nav";
 // Importing the components from index.js in sections folder
 import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality} from "./sections";
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero Section
+      <Hero/>
     </section>
     <section className="padding">
-      Popular Products
+      <PopularProducts/>
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality/>
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services/>
     </section>
     <section className="padding">
-      Special Offer
+      <SpecialOffer/>
     </section>
     <section className="bg-pale-blue padding">
-      Customer reviews
+      <CustomerReviews/>
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe/>
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      footer
+      <Footer/>
     </section>
   </main>
 );
