0f3b1a0cb3a1ca81edb7343a3be7efe5146e3d86
Roudi22 <roudi.alzkemi@gmail.com>
2023-10-22 21:49:57 +0200

    make the width dynamic

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,6 +4,7 @@ const Button = ({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth
 }) => {
   return (
     <button
@@ -14,7 +15,7 @@ const Button = ({
           : "bg-coral-red text-white border-coral-red"
       }
       
-       rounded-full`}
+       rounded-full ${fullWidth && 'w-full'}`}
     >
       {label}
       {iconURL && (
