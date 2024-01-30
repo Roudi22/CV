8a74ad874489eb3670717aff0a7a497c49265993
Roudi22 <roudi.alzkemi@gmail.com>
2024-01-30 19:48:59 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Nav from "./components/Nav";
+
 // Importing the components from index.js in sections folder
 import { Footer, Hero, PopularProducts, Services, Subscribe, SuperQuality} from "./sections";
 import { useSelector } from 'react-redux';
